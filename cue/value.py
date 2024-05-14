b8c438dc24f8d93ff0fce7e6d833b508c665b486
Aram Hăvărneanu <aram@mgk.ro>
2024-05-14 17:29:43 +0200

    cue: add Context and Context.compile
    Add `Context` class representing a CUE context along with `compile`
    function from Python str or bytes to CUE Value.
    
    Expand definition of `Value` to take both a `Context` and the
    resource from `libcue`.
    
    Make both `Context` and `Value` release Go resources when deallocated.
    
    Test `compile`. Since at the moment we can't inspect CUE value,
    this reduces to testing that compilation is either successful in
    producing a CUE value (as a Python `Value`, or errors out with an
    expected message.
    
    Fixes #3071.
    Fixes #3072.
    Updates #3100.
    Updates #3073.
    Updates #3074.
    
    Signed-off-by: Aram Hăvărneanu <aram@mgk.ro>
    Change-Id: Ib98c06aca3bcceb8dc5d3f42a3acae289bc324c0

--- cue/value.py
+++ cue/value.py
@@ -12,5 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Perform operations on CUE values.
+"""
+
+from typing import final
+import libcue
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from cue.context import Context
+
+@final
 class Value:
+    """
+    A CUE value.
+
+    Value holds any value that can be encoded by CUE.
+
+    Corresponding Go functionality is documented at:
+    https://pkg.go.dev/cuelang.org/go/cue#Value.
+    """
+
+    _ctx: 'Context'
     _val: int
+
+    def __init__(self, ctx: 'Context', v: int):
+        self._ctx = ctx
+        self._val = v
+
+    def __del__(self):
+        libcue.free(self._val)
+
+    def context(self) -> 'Context':
+        """The Context that created this Value."""
+        return self._ctx

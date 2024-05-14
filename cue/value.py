8aa01c4d100dda9671f8e7b9daa2aa334de80f12
Aram Hăvărneanu <aram@mgk.ro>
2024-05-14 17:54:41 +0200

    cue: add decoding functions from CUE values to Python types
    Updates #3100.
    Updates #3074.
    
    Signed-off-by: Aram Hăvărneanu <aram@mgk.ro>
    Change-Id: Ia4878ffec2635ce11f65e1e4f2b8d8681bab1896

--- cue/value.py
+++ cue/value.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from typing import Any, final
+from cue.error import Error
 import libcue
 
 from typing import TYPE_CHECKING
@@ -42,3 +43,100 @@ class Value:
     def unify(self, other: 'Value') -> 'Value':
         v = libcue.unify(self._val, other._val)
         return Value(self._ctx, v)
+
+    def to_int(self) -> int:
+        return _to_int(self)
+
+    def to_unsigned(self) -> int:
+        return _to_unsigned(self)
+
+    def to_bool(self) -> bool:
+        return _to_bool(self)
+
+    def to_float(self) -> float:
+        return _to_float(self)
+
+    def to_str(self) -> str:
+        return _to_str(self)
+
+    def to_bytes(self) -> bytes:
+        return _to_bytes(self)
+
+    def to_json(self) -> str:
+        return _to_json(self)
+
+def _to_int(val: Value) -> int:
+    ptr = libcue.ffi.new("int64_t*")
+    err = libcue.dec_int64(val._val, ptr)
+    if err != 0:
+        raise Error(err)
+    return ptr[0]
+
+def _to_unsigned(val: Value) -> int:
+    ptr = libcue.ffi.new("uint64_t*")
+    err = libcue.dec_uint64(val._val, ptr)
+    if err != 0:
+        raise Error(err)
+    return ptr[0]
+
+def _to_bool(val: Value) -> bool:
+    ptr = libcue.ffi.new("bool*")
+    err = libcue.dec_bool(val._val, ptr)
+    if err != 0:
+        raise Error(err)
+    return ptr[0]
+
+def _to_float(val: Value) -> float:
+    ptr = libcue.ffi.new("double*")
+    err = libcue.dec_double(val._val, ptr)
+    if err != 0:
+        raise Error(err)
+    return ptr[0]
+
+def _to_str(val: Value) -> str:
+    ptr = libcue.ffi.new("char**")
+    err = libcue.dec_string(val._val, ptr)
+    if err != 0:
+        raise Error(err)
+
+    dec = libcue.ffi.string(ptr[0])
+    if not isinstance(dec, bytes):
+        raise TypeError
+
+    s = dec.decode("utf-8")
+    libcue.libc_free(ptr[0])
+    return s
+
+def _to_bytes(val: Value) -> bytes:
+    buf_ptr = libcue.ffi.new("uint8_t**")
+    len_ptr = libcue.ffi.new("size_t*")
+
+    err = libcue.dec_bytes(val._val, buf_ptr, len_ptr)
+    if err != 0:
+        raise Error(err)
+
+    dec = libcue.ffi.buffer(buf_ptr[0], len_ptr[0])
+
+    # we slice the buffer to get a Python bytes instead of converting
+    # to bytes directly to work around the lack of precise type information
+    # in types-cffi.
+    b = dec[:]
+
+    libcue.libc_free(buf_ptr[0])
+    return b
+
+def _to_json(val: Value) -> str:
+    buf_ptr = libcue.ffi.new("uint8_t**")
+    len_ptr = libcue.ffi.new("size_t*")
+
+    err = libcue.dec_json(val._val, buf_ptr, len_ptr)
+    if err != 0:
+        raise Error(err)
+
+    dec = libcue.ffi.string(buf_ptr[0], len_ptr[0])
+    if not isinstance(dec, bytes):
+        raise TypeError
+
+    s = dec.decode("utf-8")
+    libcue.libc_free(buf_ptr[0])
+    return s
